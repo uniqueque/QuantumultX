764a5ec53e5f9c3b9dfb41aba979cefd82ae488f
quexianwei <670898080@qq.com>
2020-05-11 12:31:17 +0800

    fix 香蕉视频bug

--- Script/xjsp.js
+++ Script/xjsp.js
@@ -3,12 +3,13 @@ var obj = JSON.parse($response.body);
 if ($request.url.indexOf("/ucp/index") != -1){
   obj.data.uinfo.down_daily_remainders = "666";
   obj.data.uinfo.play_daily_remainders = "666";
+  obj.data.uinfo.minivod_play_daily_remainders="666",
+  obj.data.uinfo.minivod_down_daily_remainders="666";
   obj.data.uinfo["next_upgrade_need"] = "0";
   obj.data.user.isvip = "1";
   obj.data.user.gicon = "V5";
   obj.data.user.gid = "5";
-  obj.data.minivod_play_daily_remainders="666",
-  obj.data.minivod_down_daily_remainders="666";
+
 
 }
 if ($request.url.indexOf("/getGlobalData") != -1){
