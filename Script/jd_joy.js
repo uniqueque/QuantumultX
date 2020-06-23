f81d2ca3f9b4961d0a9ad03adfcbb0a379bcdfe5
borrring <blackspirits.zj@gmail.com>
2020-06-23 10:50:19 +0800

    宠汪汪增加自动投喂，增加完成任务推送内容（现有积分、现有狗粮、喂养次数、宠物等级）and so on... 兼容Loon和surge

--- Script/jd_joy.js
+++ Script/jd_joy.js
@@ -1,11 +1,100 @@
 //jd宠汪汪
 
-//自动投喂还没写，主要是任务完不成，不想写 了
+//0 */3 * * * jd_joy.js  #每隔三小时运行一次，加快升级
+//feedCount:自定义 每次喂养数量; 等级只和喂养次数有关，与数量无关
+//推荐每次投喂10个，积累狗粮，然后去聚宝盆赌每小时的幸运奖，据观察，投入3000-6000中奖概率大，超过7000基本上注定亏本，即使是第一名
+//Combine from Zero-S1/JD_tools(https://github.com/Zero-S1/JD_tools)
 
-//只能quanx用，request里面的请求跟获取cookie的地方改改，别的app应该也能用
+const FEED_NUM = 10   // [10,20,40,80]
+
+const $hammer = (() => {
+    const isRequest = "undefined" != typeof $request,
+        isSurge = "undefined" != typeof $httpClient,
+        isQuanX = "undefined" != typeof $task;
+
+    const log = (...n) => { for (let i in n) console.log(n[i]) };
+    const alert = (title, body = "", subtitle = "", link = "") => {
+        if (isSurge) return $notification.post(title, subtitle, body, link);
+        if (isQuanX) return $notify(title, subtitle, (link && !body ? link : body));
+        log("==============📣系统通知📣==============");
+        log("title:", title, "subtitle:", subtitle, "body:", body, "link:", link);
+    };
+    const read = key => {
+        if (isSurge) return $persistentStore.read(key);
+        if (isQuanX) return $prefs.valueForKey(key);
+    };
+    const write = (val, key) => {
+        if (isSurge) return $persistentStore.write(val, key);
+        if (isQuanX) return $prefs.setValueForKey(val, key);
+    };
+    const request = (method, params, callback) => {
+        /**
+         *
+         * params(<object>): {url: <string>, headers: <object>, body: <string>} | <url string>
+         *
+         * callback(
+         *      error,
+         *      <response-body string>?,
+         *      {status: <int>, headers: <object>, body: <string>}?
+         * )
+         *
+         */
+        let options = {};
+        if (typeof params == "string") {
+            options.url = params;
+        } else {
+            options.url = params.url;
+            if (typeof params == "object") {
+                params.headers && (options.headers = params.headers);
+                params.body && (options.body = params.body);
+            }
+        }
+        method = method.toUpperCase();
+
+        const writeRequestErrorLog = function (m, u) {
+            return err => {
+                log("=== request error -s--");
+                log(`${m} ${u}`, err);
+                log("=== request error -e--");
+            };
+        }(method, options.url);
+
+        if (isSurge) {
+            const _runner = method == "GET" ? $httpClient.get : $httpClient.post;
+            return _runner(options, (error, response, body) => {
+                if (error == null || error == "") {
+                    response.body = body;
+                    callback("", body, response);
+                } else {
+                    writeRequestErrorLog(error);
+                    callback(error);
+                }
+            });
+        }
+        if (isQuanX) {
+            options.method = method;
+            $task.fetch(options).then(
+                response => {
+                    response.status = response.statusCode;
+                    delete response.statusCode;
+                    callback("", response.body, response);
+                },
+                reason => {
+                    writeRequestErrorLog(reason.error);
+                    callback(reason.error);
+                }
+            );
+        }
+    };
+    const done = (value = {}) => {
+        if (isQuanX) return isRequest ? $done(value) : null;
+        if (isSurge) return isRequest ? $done(value) : $done();
+    };
+    return { isRequest, isSurge, isQuanX, log, alert, read, write, request, done };
+})();
 
 //直接用NobyDa的jd cookie
-const cookie = $prefs.valueForKey('CookieJD')
+const cookie = $hammer.read('CookieJD')
 const name = '京东宠汪汪'
 
 var Task = step();
@@ -52,10 +141,9 @@ function* step() {
                     if (!market.status) {
                         let clickResult = yield click(market.marketLink)
                         console.log(`逛会场点击${market.marketName}结果${JSON.stringify(clickResult)}`)
-
+                        
                         let scanMarketResult = yield ScanMarket(market.marketLink)
                         console.log(`逛会场${market.marketName}结果${JSON.stringify(scanMarketResult)}`)
-
                     }
                 }
             } else {
@@ -83,7 +171,7 @@ function* step() {
                     }
                 }
             } else {
-                console.log(`浏览频道今天已完成或任务不存在`)
+                console.log(`浏览商品今天已完成或任务不存在`)
             }
             //浏览商品奖励积分
             let deskGoodDetails = yield getDeskGoodDetails()
@@ -91,12 +179,19 @@ function* step() {
                 for (let deskGood of deskGoodDetails.data.deskGoods) {
                     if (!deskGood.status) {
                         let scanDeskGoodResult = yield ScanDeskGood(deskGood.sku)
-                        console.log(`浏览商品${deskGood.skuName}结果${JSON.stringify(scanDeskGoodResult)}`)
+                        console.log(`浏览频道${deskGood.skuName}结果${JSON.stringify(scanDeskGoodResult)}`)
                     }
                 }
             } else {
                 console.log(`浏览商品奖励积分返回结果${JSON.stringify(deskGoodDetails)}`)
             }
+            // 喂食
+            let feedPetsResult = yield feedPets()
+            console.log(`喂食结果${JSON.stringify(feedPetsResult)}`)
+            // 喂养状态
+            let enterRoomResult = yield enterRoom()
+            console.log(`喂养状态${JSON.stringify(enterRoomResult)}`)
+            message = `现有积分: ${enterRoomResult.data.petCoin}\n现有狗粮: ${enterRoomResult.data.petFood}\n喂养次数: ${enterRoomResult.data.feedCount}\n宠物等级: ${enterRoomResult.data.petLevel}`
         } else {
             console.log(`任务信息${JSON.stringify(petTaskConfig)}`)
             message = petTaskConfig.errorMessage
@@ -104,13 +199,12 @@ function* step() {
     } else {
         message = '请先获取cookie\n直接使用NobyDa的京东签到获取'
     }
-    $notify(name, '', message)
+    $hammer.alert(name, message, '')
 }
 
 function click(marketLink) {
     request(`https://jdjoy.jd.com/pet/icon/click?reqSource=h5&iconCode=scan_market&linkAddr=${marketLink}`)
 }
-
 
 //浏览商品
 function ScanDeskGood(sku) {
@@ -133,7 +227,7 @@ function followGood(sku) {
 }
 
 //逛会场
-function ScanMarket(marketLink) {
+function ScanMarket(marketLink,) {
     requestPost(`https://jdjoy.jd.com/pet/scan`, JSON.stringify({ marketLink: marketLink, taskType: 'ScanMarket', reqSource: 'h5' }), 'application/json')
 }
 //关注店铺
@@ -154,38 +248,45 @@ function ThreeMeals() {
     request(`https://jdjoy.jd.com/pet/getFood?taskType=ThreeMeals`)
 }
 
+//喂食
+function feedPets() {
+    request(`https://jdjoy.jd.com/pet/feed?feedCount=${FEED_NUM}`)
+}
+
+//喂养状态
+function enterRoom() {
+    request(`https://jdjoy.jd.com/pet/enterRoom?reqSource=h5`)
+}
+
 function request(url) {
-    $task.fetch({
+    $hammer.log("request url:", url);
+    const option =  {
         url: url,
         headers: {
             Cookie: cookie,
             reqSource: 'h5',
-        },
-        method: "GET",
-    }).then(
-        (response) => {
-            return JSON.parse(response.body)
-        },
-        (reason) => console.log(reason.error, reason)
-    ).then((response) => sleep(response))
+        }
+    };
+    $hammer.request('GET', option, (error, response) => {
+        error ? $hammer.log("Error:", error) : sleep(JSON.parse(response));
+    })
 }
 
 function requestPost(url, body, ContentType) {
-    $task.fetch({
+    $hammer.log("request url:", url, "body:", body, "ContetentType:", ContentType);
+    const options = {
         url: url,
         body: body,
         headers: {
             Cookie: cookie,
+        UserAgent: `Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1`,
             reqSource: 'h5',
             'Content-Type': ContentType,
-        },
-        method: "POST",
-    }).then(
-        (response) => {
-            return JSON.parse(response.body)
-        },
-        (reason) => console.log(reason.error, reason)
-    ).then((response) => sleep(response))
+        }
+    };
+    $hammer.request('POST', options, (error, response) => {
+        error ? $hammer.log("Error:", error) : sleep(JSON.parse(response));
+    })
 }
 
 function sleep(response) {
